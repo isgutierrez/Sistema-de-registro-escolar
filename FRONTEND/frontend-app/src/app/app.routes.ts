83cff340d860e08d782b4cd878262a2a36ee466d
Isabella <igutierrezmier@gmail.com>
2025-05-18 15:53:42 -0500

    Se agregan componentes reutilizables

--- FRONTEND/frontend-app/src/app/app.routes.ts
+++ FRONTEND/frontend-app/src/app/app.routes.ts
@@ -4,6 +4,9 @@ import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { PersonasComponent } from './pages/personas/personas.component';
 import { EstudiantesComponent } from './pages/estudiantes/estudiantes.component';
 import { ProfesoresComponent } from './pages/profesores/profesores.component';
+import { AdministrativosComponent } from './pages/administrativos/administrativos.component';
+import { CursosComponent } from './pages/cursos/cursos.component';
+import { InscripcionesComponent } from './pages/inscripciones/inscripciones.component';
 
 export const routes: Routes = [
     {
@@ -11,7 +14,7 @@ export const routes: Routes = [
         component: MainComponent,
         children: [
             {
-                path: '',
+                path: 'dashboard',
                 component: DashboardComponent,
             },
             {
@@ -26,6 +29,18 @@ export const routes: Routes = [
                 path: 'profesores',
                 component: ProfesoresComponent,
             },
+            {
+                path: 'administrativos',
+                component: AdministrativosComponent,
+            },
+            {
+                path: 'cursos',
+                component: CursosComponent,
+            },
+            {
+                path: 'inscripciones',
+                component: InscripcionesComponent,
+            }
         ]
     }
 ];
---
+++ FRONTEND/frontend-app/src/app/components/modalform/modalform.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modalform',
+  standalone: true,
+  imports: [],
+  templateUrl: './modalform.component.html',
+  styleUrl: './modalform.component.css'
+})
+export class ModalformComponent {
+
+}
